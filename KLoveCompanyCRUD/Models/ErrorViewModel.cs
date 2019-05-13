5e865b0c3aed88d42a37f02f74aeace5eef5d37a
DESKTOP-MIHSSDS\Raffi <raffivegas@gmail.com>
2019-05-13 16:08:57 -0700

    initialize.

---
+++ KLoveCompanyCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KLoveCompanyCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
