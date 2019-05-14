abd04203d2f31019b11acb2ab5fc801027ac6fdc
DESKTOP-MIHSSDS\Raffi <raffivegas@gmail.com>
2019-05-13 20:20:25 -0700

    throwing stuff at the wall, hoping something sticks.  I'm thinking I'll continue with this view model and tweak it so it can list whats in the tables with their relationships.  I'll have a separate view which will add employees?

---
+++ KLoveCompanyCRUD/Controllers/SummaryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KLoveCompanyCRUD.Models;
+using KLoveCompanyCRUD.View_Models;
+
+namespace KLoveCompanyCRUD.Controllers
+{
+    public class SummaryController : Controller
+    {
+        private readonly KLoveCompanyCRUDContext _context;
+
+        private SummaryVM viewModelSummary = new SummaryVM();
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public SummaryController(KLoveCompanyCRUDContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employee.ToListAsync();
+            var departments = await _context.Department.ToListAsync();
+            viewModelSummary.AddressLine1 = employees[0].AddressLine1;
+            viewModelSummary.Name = departments[0].Name;
+            return View(viewModelSummary);
+        }
+    }
+}
---
+++ KLoveCompanyCRUD/View Models/SummaryVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KLoveCompanyCRUD.View_Models
+{
+    public class SummaryVM
+    {
+        public int EId { get; set; }
+        public int DepartmentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Email1 { get; set; }
+        public string Email2 { get; set; }
+
+        public int DId { get; set; }
+        public string Name { get; set; }
+
+        public SummaryVM()
+        {
+           
+        }
+    }
+
+}
