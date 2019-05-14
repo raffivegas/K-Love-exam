770fce5fb3602cf9737d8e337e15774b0126b7e0
DESKTOP-MIHSSDS\Raffi <raffivegas@gmail.com>
2019-05-14 00:12:12 -0700

    figuring some way to get the department name for a new employee.

---
+++ KLoveCompanyCRUD/Controllers/AddJoinedEmployeeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KLoveCompanyCRUD.Models;
+using KLoveCompanyCRUD.View_Models;
+
+namespace KLoveCompanyCRUD.Controllers
+{
+    public class AddJoinedEmployeeController : Controller
+    {
+        private readonly KLoveCompanyCRUDContext _context;
+
+        //public List<SummaryVM> viewModelSummaryList = new List<SummaryVM>();
+
+        //public SummaryVM testRaf = new SummaryVM();
+
+        public AddEmployeeVM viewModelAddEmployee = new AddEmployeeVM();
+
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public AddJoinedEmployeeController(KLoveCompanyCRUDContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employee.ToListAsync();
+            var departments = await _context.Department.ToListAsync();
+
+            var listOfDepartments = from department in departments
+                                    select new
+                                    {
+                                        DepartmentName = department.Name
+                                    };
+
+
+            //not pretty, but will do for now.  Git err dun.
+            foreach (var row in listOfDepartments)
+            {
+                viewModelAddEmployee.Departments.Add(row.DepartmentName);
+            }
+
+            //return View(viewModelSummaryList);
+            return View(viewModelAddEmployee);
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("FirstName,LastName,AddressLine1,AddressLine2,City,State,Zip,Email1,Email2")] Employee employee)
+        public async Task<IActionResult> Create(AddEmployeeVM viewModel, [Bind("FirstName,LastName,AddressLine1,AddressLine2,City,State,Zip,Email1,Email2")] Employee employee)
+        {
+            string tst = Request.Form["Name"];
+            if (ModelState.IsValid)
+            {
+                //Employee employee = new Employee();
+                //employee.AddressLine1 = 
+                //var test = viewModel.Name;
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
---
+++ KLoveCompanyCRUD/View Models/AddEmployeeVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KLoveCompanyCRUD.View_Models
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+        public class AddEmployeeVM /*: IEnumerable<SummaryVM>*/
+        {
+            public int EId { get; set; }
+            public int DepartmentId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string AddressLine1 { get; set; }
+            public string AddressLine2 { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Zip { get; set; }
+            public string Email1 { get; set; }
+            public string Email2 { get; set; }
+
+            public int DId { get; set; }
+            public string Name { get; set; }
+
+            public List<string> Departments { get; set; }
+
+            public AddEmployeeVM()
+            {
+                Departments = new List<string>();
+            }
+
+            //public IEnumerator<SummaryVM> GetEnumerator()
+            //{
+            //    return summaryVMs.GetEnumerator();
+            //    //throw new NotImplementedException();
+            //}
+
+            //IEnumerator IEnumerable.GetEnumerator()
+            //{
+            //    return GetEnumerator();
+            //    //throw new NotImplementedException();
+            //}
+        }
+
+    }
+
+
