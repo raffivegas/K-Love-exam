a6b224f8e606143b7bc4acd6e9deb3fcf7b4bab5
DESKTOP-MIHSSDS\Raffi <raffivegas@gmail.com>
2019-05-13 17:46:26 -0700

    Departments CRUD working.

---
+++ KLoveCompanyCRUD/Data/KLoveCompanyCRUDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KLoveCompanyCRUD.Models;
+
+namespace KLoveCompanyCRUD.Models
+{
+    public class KLoveCompanyCRUDContext : DbContext
+    {
+        public KLoveCompanyCRUDContext (DbContextOptions<KLoveCompanyCRUDContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<KLoveCompanyCRUD.Models.Department> Department { get; set; }
+
+    }
+}
